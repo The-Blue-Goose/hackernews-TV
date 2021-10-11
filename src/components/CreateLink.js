2bb5e2b7e825bb20a763db95da11a21ae4173305
Trey <42703682+The-Blue-Goose@users.noreply.github.com>
2021-10-11 14:25:53 -0400

    moreinfo

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -39,8 +39,8 @@ class CreateLink extends Component {
           />
         </div>
         <Mutation mutation={POST_MUTATION} variables={{ description, url }}>
-          {() => (
-            <button onClick={`... you'll implement this 🔜`}>
+          {postMutation =>  (
+            <button onClick={postMutation}>
               Submit
             </button>
           )}
