18c4eb8c89c4a1428fe9eff151df46b35b072ddb
Trey <42703682+The-Blue-Goose@users.noreply.github.com>
2021-10-11 14:08:13 -0400

    link

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -1,101 +1,53 @@
-import React, { useState } from 'react';
-import { gql, useMutation } from '@apollo/client';
-import { useHistory } from 'react-router';
-import { LINKS_PER_PAGE } from '../constants';
-import { FEED_QUERY } from './LinkList';
+import React, { Component } from 'react'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
 
-const CREATE_LINK_MUTATION = gql`
-  mutation PostMutation(
-    $description: String!
-    $url: String!
-  ) {
+const POST_MUTATION = gql`
+  mutation PostMutation($description: String!, $url: String!) {
     post(description: $description, url: $url) {
       id
+      createdAt
       url
       description
     }
   }
-`;
-
-const CreateLink = () => {
-  const history = useHistory();
-  const [formState, setFormState] = useState({
+`
+class CreateLink extends Component {
+  state = {
     description: '',
-    url: ''
-  });
-  const [createLink] = useMutation(CREATE_LINK_MUTATION, {
-    variables: {
-      description: formState.description,
-      url: formState.url
-    },
-    update: (cache, { data: { post } }) => {
-      const take = LINKS_PER_PAGE;
-      const skip = 0;
-      const orderBy = { createdAt: 'desc' };
+    url: '',
+  }
 
-      const data = cache.readQuery({
-        query: FEED_QUERY,
-        variables: {
-          take,
-          skip,
-          orderBy
-        }
-      });
-
-      cache.writeQuery({
-        query: FEED_QUERY,
-        data: {
-          feed: {
-            links: [post, ...data.feed.links]
-          }
-        },
-        variables: {
-          take,
-          skip,
-          orderBy
-        }
-      });
-    },
-    onCompleted: () => history.push('/new/1')
-  });
-  return (
-    <div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          createLink();
-        }}
-      >
+  render() {
+    const { description, url } = this.state
+    return (
+      <div>
         <div className="flex flex-column mt3">
           <input
             className="mb2"
-            value={formState.description}
-            onChange={(e) =>
-              setFormState({
-                ...formState,
-                description: e.target.value
-              })
-            }
+            value={description}
+            onChange={e => this.setState({ description: e.target.value })}
             type="text"
             placeholder="A description for the link"
           />
           <input
             className="mb2"
-            value={formState.url}
-            onChange={(e) =>
-              setFormState({
-                ...formState,
-                url: e.target.value
-              })
-            }
+            value={url}
+            onChange={e => this.setState({ url: e.target.value })}
             type="text"
             placeholder="The URL for the link"
           />
         </div>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
-  );
-};
+        <Mutation mutation={POST_MUTATION} variables={{ description, url }}>
+          {() => (
+            <button onClick={`... you'll implement this 🔜`}>
+              Submit
+            </button>
+          )}
+        </Mutation>
+      </div>
+    )
+  }
+}
 
-export default CreateLink;
+export default CreateLink
