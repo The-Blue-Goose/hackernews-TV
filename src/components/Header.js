7733de982aec7e129d0c2982afded837d1d14fb6
Trey <42703682+The-Blue-Goose@users.noreply.github.com>
2021-10-12 14:10:57 -0400

    render

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,63 +1,24 @@
-import React from 'react';
-import { useHistory } from 'react-router';
-import { Link } from 'react-router-dom';
-import { AUTH_TOKEN } from '../constants';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { withRouter } from 'react-router'
 
-const Header = () => {
-  const history = useHistory();
-  const authToken = localStorage.getItem(AUTH_TOKEN);
-  return (
-    <div className="flex pa1 justify-between nowrap orange">
-      <div className="flex flex-fixed black">
-        <div className="fw7 mr1">Hacker News</div>
-        <Link to="/" className="ml1 no-underline black">
-          new
-        </Link>
-        <div className="ml1">|</div>
-        <Link to="/top" className="ml1 no-underline black">
-          top
-        </Link>
-        <div className="ml1">|</div>
-        <Link
-          to="/search"
-          className="ml1 no-underline black"
-        >
-          search
-        </Link>
-        {authToken && (
-          <div className="flex">
-            <div className="ml1">|</div>
-            <Link
-              to="/create"
-              className="ml1 no-underline black"
-            >
-              submit
-            </Link>
-          </div>
-        )}
-      </div>
-      <div className="flex flex-fixed">
-        {authToken ? (
-          <div
-            className="ml1 pointer black"
-            onClick={() => {
-              localStorage.removeItem(AUTH_TOKEN);
-              history.push(`/`);
-            }}
-          >
-            logout
-          </div>
-        ) : (
-          <Link
-            to="/login"
-            className="ml1 no-underline black"
-          >
-            login
+class Header extends Component {
+  render() {
+    return (
+      <div className="flex pa1 justify-between nowrap orange">
+        <div className="flex flex-fixed black">
+          <div className="fw7 mr1">Hacker News</div>
+          <Link to="/" className="ml1 no-underline black">
+            new
           </Link>
-        )}
+          <div className="ml1">|</div>
+          <Link to="/create" className="ml1 no-underline black">
+            submit
+          </Link>
+        </div>
       </div>
-    </div>
-  );
-};
+    )
+  }
+}
 
-export default Header;
+export default withRouter(Header)
--- src/index.js
+++ src/index.js
@@ -2,26 +2,42 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/index.css'
 import App from './components/App'
-//import './serviceWorkerRegistration'
-//import * as serviceWorker from './service-worker'
+//import registerServiceWorker from './registerServiceWorker'
 import { ApolloProvider } from 'react-apollo'
 import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { setContext } from 'apollo-link-context'
+import { BrowserRouter } from 'react-router-dom'
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 })
-
+/*
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache()
-})
+}) */
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjMsImlhdCI6MTYzMzM3OTIxMX0.HeuBGiu_2bwzIosaLK73693AbedF14EyCWpLo4KDAOg',
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 )
-//ServiceWorkerRegistration()
+//registerServiceWorker()
