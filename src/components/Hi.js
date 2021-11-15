fd037da5abfc5cfda99f2a1d77253c80184ae29b
Trey <42703682+The-Blue-Goose@users.noreply.github.com>
2021-11-15 14:25:19 -0500

    Hi

---
+++ src/components/Hi.js
@@ -0,0 +1,14 @@
+import React, { component } from 'react'
+import { AUTH_TOKEN } from '../constants'
+
+const name = 'William H. Cerhelle III';
+
+class Hi extends component {
+    render() {
+        //const authToken = localStorage.getItem(AUTH_TOKEN)
+        return(
+            <h1 className="Hello">My name is {name}</h1>
+        )
+    }
+}
+export default Hi
