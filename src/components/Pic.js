67ab7b49bde57252b885e61401d63d1f726a6101
Trey <42703682+The-Blue-Goose@users.noreply.github.com>
2022-01-25 14:17:08 -0500

    PicFix Profecionalise

--- src/components/Pic.js
+++ src/components/Pic.js
@@ -4,15 +4,14 @@ class Pic extends Component {
     return (
       <div className="flex mt2 items-start">
         <div className="flex items-center">
-  <span className="yellow">{this.props.index + 1}.</span>
+  <span className="gray">{this.props.index + 1}.</span>
 </div>
-
         <div className="ml1">
-          <div><img src={this.props.pic.url} alt={this.props.pic.description}></img></div>
+        <div><img src={this.props.pic.url} alt={this.props.pic.description} width="500" height="~"/></div>
           <div>
           <a href={this.props.pic.url} target="_blank" rel="noreferrer">{this.props.pic.description}</a>
           </div>
-          <div className="f6 lh-copy green">
+          <div className="f6 lh-copy gray">
               by{' '}
             {this.props.pic.postedBy
               ? this.props.pic.postedBy.name
